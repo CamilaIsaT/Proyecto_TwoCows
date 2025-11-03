3ace9138b2b07ab8d1e662094073281b0210d28a
Stephanie Santamaria <stephmariurbina32@gmail.com>
2025-11-02 19:36:40 -0600

    serializer de auth

--- twocows/serializer.py
+++ twocows/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 # Crear un serializador para el modelo vacas
@@ -11,3 +12,22 @@ class vacasSerializer(serializers.ModelSerializer):
         # Incluir todos los campos del modelo vacas
         fields = '__all__'
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username','email']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
